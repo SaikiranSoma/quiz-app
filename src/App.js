73fc07374513ebac06da940b0a73c0f0fba14987
SaikiranSoma <saikiransoma98@gmail.com>
2025-03-27 16:38:20 +0530

    usereduce state added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DateCounter from './DateCounter';
 
 function App() {
   return (
     <>
-                  
+       <DateCounter/>   
     </>
   );
 }
